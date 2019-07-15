cf73e69e55b9f6ed2daf594d0bf4e9e8417507fe
Akin Odunlami <akin.odunlami@analogfolk.com>
2019-07-15 21:13:37 +0100

    add lodash and some tests + dev dependencies

---
+++ test/string.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var _ = require('lodash');
+var assert = chai.assert;
+
+describe('testing lodash string features', function() {
+    it('testing string to camelcase', function () {
+        let res = _.camelCase('dolapo odunlami');
+        assert.isString(res);
+        assert.equal(res, 'dolapoOdunlami')
+    });
+
+    it('testing string to uppercase', function () {
+        let res = _.toUpper('dolapo odunlami');
+        assert.isString(res);
+        assert.equal(res, 'DOLAPO ODUNLAMI')
+    });
+
+    it('testing string to capitalize', function () {
+        let res = _.capitalize('dolapo odunlami');
+        assert.isString(res);
+        assert.equal(res, 'Dolapo odunlami')
+    })
+});
