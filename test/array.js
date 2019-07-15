587ec46555f8c3f851d2dc10d5a1be1480e20717
Akin Odunlami <akin.odunlami@analogfolk.com>
2019-07-16 00:33:24 +0100

    linted tests

--- test/array.js
+++ test/array.js
@@ -1,39 +1,39 @@
-var chai = require('chai');
-var _ = require('lodash');
-var assert = chai.assert;
+var chai = require('chai')
+var _ = require('lodash')
+var assert = chai.assert
 
 describe('testing lodash array features', function() {
     it('testing intersection of arrays', function () {
-        let res = _.intersection([2, 1], [2, 3]);
-        assert.isArray(res);
+        let res = _.intersection([2, 1], [2, 3])
+        assert.isArray(res)
         assert.sameOrderedMembers(res, [2])
-    });
+    })
 
     it('testing union of arrays', function () {
-        let res = _.union([2], [1, 2]);
-        assert.isArray(res);
+        let res = _.union([2], [1, 2])
+        assert.isArray(res)
         assert.sameOrderedMembers(res, [2, 1])
-    });
+    })
 
     it('testing chunking of arrays', function () {
-        let res = _.chunk([3,4,5,5,6,2,1,7,8,3,1,9,3,6], 3);
-        assert.isArray(res);
-        assert.lengthOf(res, 5);
-        assert.sameOrderedMembers(res[0], [3,4,5]);
+        let res = _.chunk([3,4,5,5,6,2,1,7,8,3,1,9,3,6], 3)
+        assert.isArray(res)
+        assert.lengthOf(res, 5)
+        assert.sameOrderedMembers(res[0], [3,4,5])
         assert.sameOrderedMembers(res[4], [3,6])
-    });
+    })
 
     it('testing compacting of arrays', function () {
-        let res = _.compact([false, 2, '3', "", '', undefined, null, 'a', 'v']);
-        assert.isArray(res);
-        assert.lengthOf(res, 4);
+        let res = _.compact([false, 2, '3', '', '', undefined, null, 'a', 'v'])
+        assert.isArray(res)
+        assert.lengthOf(res, 4)
         assert.sameOrderedMembers(res, [2, '3', 'a', 'v'])
-    });
+    })
 
     it('testing concating of arrays', function () {
-        let res = _.concat(['dolapo'], [2,3,4], ['a'], ['d', 'b']);
-        assert.isArray(res);
-        assert.lengthOf(res, 7);
+        let res = _.concat(['dolapo'], [2,3,4], ['a'], ['d', 'b'])
+        assert.isArray(res)
+        assert.lengthOf(res, 7)
         assert.sameOrderedMembers(res, ['dolapo', 2, 3, 4, 'a', 'd', 'b'])
     })
-});
+})
--- test/collection.js
+++ test/collection.js
@@ -3,21 +3,21 @@ const _ = require('lodash')
 const assert = chai.assert
 
 describe('testing lodash collection features', function () {
-  it('testing countby on collections', function () {
-    const res = _.countBy(['one', 'two', 'three'], 'length')
-    assert.propertyVal(res, '3', 2)
-    assert.propertyVal(res, '5', 1)
-  })
+    it('testing countby on collections', function () {
+        const res = _.countBy(['one', 'two', 'three'], 'length')
+        assert.propertyVal(res, '3', 2)
+        assert.propertyVal(res, '5', 1)
+    })
 
-  it('testing every on collections', function () {
-    const res = _.every([2, 4, 'a', 'ddd'], Number)
-    assert.isFalse(res)
-  })
+    it('testing every on collections', function () {
+        const res = _.every([2, 4, 'a', 'ddd'], Number)
+        assert.isFalse(res)
+    })
 
-  it('testing filter on collections', function () {
-    const res = _.filter([2, 4, 'a', 'ddd'], function (ele) { return !isNaN(ele) })
-    assert.isArray(res)
-    assert.lengthOf(res, 2)
-    assert.sameOrderedMembers(res, [2, 4])
-  })
+    it('testing filter on collections', function () {
+        const res = _.filter([2, 4, 'a', 'ddd'], function (ele) { return !isNaN(ele) })
+        assert.isArray(res)
+        assert.lengthOf(res, 2)
+        assert.sameOrderedMembers(res, [2, 4])
+    })
 })
